974d76b4d0a400c2d714d33ded855a2b4825a7e8
Kevoharvey$ <Kevin.Harvey2006@gmail.com>
2026-05-03 22:37:59 +0300

    chore: remove hardcoded database password and delete unused employment date field from signup form

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ FRONTEND_DIR = "templates"
 DB_CONFIG = {
     "host": os.environ.get("DB_HOST", "localhost"),
     "user": os.environ.get("DB_USER", "root"),
-    "password": os.environ.get("DB_PASSWORD", "MySQL_12345"),
+    "password": os.environ.get("DB_PASSWORD", ""),
     "database": os.environ.get("DB_NAME", "school_db"),
     "port": int(os.environ.get("DB_PORT", "3306")),
 }
